1531b8f8d955e170105db558fd7521ffba5847cd
Marcus Flores <flresmarcus@gmail.com>
2021-08-26 15:01:25 -0700

    Added broken scroll to top function, will fix later

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {AppBar, ThemeProvider, Toolbar, Button} from '@material-ui/core';
+import {AppBar, ThemeProvider, Toolbar, Button, useScrollTrigger, CssBaseline, Zoom, Fab} from '@material-ui/core';
+import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp';
 import { theme } from './Theme';
 import { makeStyles } from '@material-ui/core';
 import { Link } from 'react-router-dom';
@@ -9,15 +10,44 @@ const mainTheme = theme;
 const useStyles = makeStyles((theme) =>  ({
     root: {
         flexGrow: 1
+        
     },
     spacer: {
         flexGrow: 1
+    },
+    scrollOver: {
+        bottom: theme.spacing(2),
+        right: theme.spacing(2)
     }
 }));
 
+function ScrollTop(props){
+    const {children} = props;
+    const classes = useStyles();
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const onClick = (event) => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#scroll-top-anchor');
+
+        if (anchor) {
+            anchor.scrollIntoView({ behavior: 'smooth', block: 'center'});
+        }
+    };
+
+    return (
+        <Zoom in={trigger}>
+            <div onClick={onClick} role="presentation" className={classes.scrollOver}>
+                {children}
+            </div>
+        </Zoom>
+    );
+}
 
 
-function Nav(){
+function Nav(props){
     const classes = useStyles();
 
     const linkStyle = {
@@ -27,6 +57,7 @@ function Nav(){
 
     return (
         <div className = {classes.root}>
+            <CssBaseline />
             <ThemeProvider theme = {mainTheme}>
                 <AppBar position="static" color="primary">
                     <Toolbar>
@@ -42,6 +73,12 @@ function Nav(){
                         </Link>
                     </Toolbar>
                 </AppBar>
+                <Toolbar id="#scroll-top-anchor"/>
+                <ScrollTop {...props}>
+                    <Fab color = "primary" size ="small" aria-label="scroll back to top">
+                        <KeyboardArrowUp />
+                    </Fab>
+                </ScrollTop>
             </ThemeProvider>
         </div>
     );
