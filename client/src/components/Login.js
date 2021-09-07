6a6245c9586df1f5dbdce14f0fa679187df6aa4e
Marcus Flores <flresmarcus@gmail.com>
2021-09-06 20:46:38 -0700

    added simple profile authentication with Auth0

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../App.css';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
 
 function Login(){
     return (
         <div>
             <h1>Login Page</h1>
+            <LoginButton/>
+            <LogoutButton/>
+            <Profile/>
         </div>
     );
 }
