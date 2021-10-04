aafde938de57d06cd9a7a34ad6aac0c610f0928f
Marcus Flores <flresmarcus@gmail.com>
2021-10-04 16:11:32 -0700

    added some todo stuff, set up path to success :)

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -32,6 +32,7 @@ const Counter = () => {
                 </Button>
                 <div style= {{margin: "10px"}} className = "counter"> {user.name}'s Score: {count}</div>
                 <h1>Data</h1>
+                {/*TODO:This function can be used to set up the leaderboard as it already renders the information to the app, all that needs to happen now is to only add a new entry to the database if its a user's first time updating score, otherwise don't create a new entry just update the previous entry associated with the id and useEffect to update on the frontend counter*/}
                 {scoreList.map((val, key) => {
                     return <div><h1>username: {val.username} scores: {val.counter}</h1> </div>
                 })}
